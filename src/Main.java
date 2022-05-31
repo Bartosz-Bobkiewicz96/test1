f97610dad6c8ada9a5d83888ea996266d8a0b3cb
barto <bartosz.bobkiewicz1@gmail.com>
2022-05-31 21:00:37 +0200

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,16 @@
+import java.util.HashMap;
+import java.util.Map;
+
+
+    public class Main {
+
+        public static void main(String[] args) {
+            Map<String, String> nameToMeal = new HashMap<>();
+            nameToMeal.put("Mariusz", "Pizza");
+            nameToMeal.put("Dominik", "Burger");
+            nameToMeal.put("Paweł", "Salad");
+
+            System.out.println(nameToMeal.get("Dominik"));
+        }
+    }
+
